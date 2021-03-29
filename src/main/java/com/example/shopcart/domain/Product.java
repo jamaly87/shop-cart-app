b1bfc6402eb4185019088d9e2abfc6efaa667202
Alhamza <h.aljamaly@gmail.com>
2021-03-29 19:24:20 -0400

    Created a Product Repository; Updated configurations to presist data in h2

--- src/main/java/com/example/shopcart/domain/Product.java
+++ src/main/java/com/example/shopcart/domain/Product.java
@@ -10,7 +10,6 @@ import javax.persistence.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Product {
@@ -18,9 +17,14 @@ public class Product {
     @SequenceGenerator(name = "product_sequence",sequenceName = "product_sequence",
                         allocationSize = 1)
     @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "product_sequence")
-    private int Id;
+    private Long Id;
     private String name;
     private String description;
     transient Price price; //TODO - check whether to keep or remove 'transient'
 
+    public Product(String name, String description, Price price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 }
